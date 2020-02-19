32c80468162954413e8af0a13979abcb149cac89
James Mulligan <james.mulligan@berkeley.edu>
2020-02-18 22:22:16 -0800

    implement generic parameter scan functionality

--- jetscape_analysis/generate/jetscape_events.py
+++ jetscape_analysis/generate/jetscape_events.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 """
-  Class to launch the generation of JETSCAPE events over a set of pt-hat bins.
-  (You can easily adapt this to loop over any set of JETSCAPE parameter values).
+  Class to launch the generation of JETSCAPE events over a set of pt-hat bins,
+  and optionally over any set of additional JETSCAPE parameter values.
+  If additional parameter values are specified, all combinations of the
+  specified parameter values will be generated.
 
   Run from inside the JETSCAPE docker container with:
     python generate_jetscape_events.py -c /home/jetscape-user/JETSCAPE-analysis/config/jetscapeAnalysisConfig.yaml -o /my/outputdir
@@ -19,8 +21,10 @@ import shutil
 import subprocess
 import sys
 import yaml
+import itertools
 
 # Base class
+sys.path.append('../..')
 from jetscape_analysis.base import common_base
 
 ################################################################
@@ -55,33 +59,61 @@ class generate_jetscape_events(common_base.common_base):
 
         self.debug_level = config["debug_level"]
 
-        self.pt_hat_bins = config["pt_hat_bins"]
-
         self.xml_user_file = config["xml_user_file"]
         self.xml_master_file = config["xml_master_file"]
+                
+        self.parameter_scan_dict = config['parameter_scan']
+        self.pt_hat_bins = self.parameter_scan_dict['pt_hat_bins']['values']
 
     # ---------------------------------------------------------------
     # Main processing function
     # ---------------------------------------------------------------
     def generate_jetscape_events(self):
 
-        # Loop through pT-hat bins and create directory structure with XML files for each bin
-        for bin, pt_hat_min in enumerate(self.pt_hat_bins):
+        # Store list of parameter labels and formatting
+        parameter_labels = [self.parameter_scan_dict[key]['label'] for key in self.parameter_scan_dict]
+        parameter_spacings = [self.parameter_scan_dict[key]['spacing'] for key in self.parameter_scan_dict]
 
-            # Set min,max of pT-hat bin
-            if bin < (len(self.pt_hat_bins) - 1):
-                pt_hat_max = self.pt_hat_bins[bin + 1]
+        # Create list of all combinations of parameters
+        parameter_values = [self.parameter_scan_dict[key]['values'] for key in self.parameter_scan_dict]
+        parameter_combinations = list(itertools.product(*parameter_values))
+        
+        # Remove that last pt-hat bin edge
+        n_combinations_per_pthat = int(len(parameter_combinations)/len(self.pt_hat_bins))
+        parameter_combinations = parameter_combinations[:-n_combinations_per_pthat]
+
+        # Loop through all parameter combinations
+        for index, parameter_combination in enumerate(parameter_combinations):
+        
+            pt_hat_bin = int(index / n_combinations_per_pthat)
+            if pt_hat_bin < len(self.pt_hat_bins) - 1:
+                pt_hat_min = self.pt_hat_bins[pt_hat_bin]
+                pt_hat_max = self.pt_hat_bins[pt_hat_bin + 1]
             else:
                 continue
+            if index % n_combinations_per_pthat == 0:
+                print('Generating pt-hat: {} - {} ...'.format(pt_hat_min, pt_hat_max))
 
+            # Create label for output directory
+            dir_label = ''
+            for index, value in enumerate(parameter_combination):
+                if index == 0:
+                    continue
+                if index != 1:
+                    dir_label += '_'
+                dir_label += parameter_labels[index]
+                dir_label += str(value)
+            if len(parameter_combination) > 1:
+                print('    Generating {}'.format(dir_label))
+                
             # Create outputDir for each bin
-            output_dir_bin = "{}{}".format(self.output_dir, bin)
+            output_dir_bin = '{}{}/{}'.format(self.output_dir, pt_hat_bin, dir_label)
             if not output_dir_bin.endswith("/"):
                 output_dir_bin = output_dir_bin + "/"
             if not os.path.exists(output_dir_bin):
                 os.makedirs(output_dir_bin)
 
-            # Copy XML files to pt-hat bin directory 
+            # Copy XML files to pt-hat bin directory
             xml_master_file_copy = "{}{}".format(output_dir_bin, "jetscape_master.xml")
             cmd = "rsync {} {}".format(self.xml_master_file, xml_master_file_copy)
             os.system(cmd)
@@ -89,37 +121,44 @@ class generate_jetscape_events(common_base.common_base):
             xml_user_file_copy = "{}{}".format(output_dir_bin, "jetscape_user.xml")
             cmd = "rsync {} {}".format(self.xml_user_file, xml_user_file_copy)
             os.system(cmd)
-
-            # Set pT-hat values in Jetscape User XML configuration
-            for line in fileinput.input(xml_user_file_copy, inplace=True):
-                if "pTHatMin" in line:
-                    print("      <pTHatMin>{}</pTHatMin>".format(pt_hat_min))
-                elif "pTHatMax" in line:
-                    print("      <pTHatMax>{}</pTHatMax>".format(pt_hat_max))
+                    
+            # Set parameters in the Jetscape User XML configuration
+            for index, value in enumerate(parameter_combination):
+            
+                parameter_label = parameter_labels[index]
+                parameter_spacing = parameter_spacings[index]
+            
+                # Set pt-hat
+                if parameter_label == 'pt_hat_bins':
+                
+                    for line in fileinput.input(xml_user_file_copy, inplace=True):
+                
+                        if 'pTHatMin' in line:
+                            print('{}<pTHatMin>{}</pTHatMin>'.format(parameter_spacing, pt_hat_min))
+                        elif 'pTHatMax' in line:
+                            print('{}<pTHatMax>{}</pTHatMax>'.format(parameter_spacing, pt_hat_max))
+                        else:
+                            print(line, end='')
+                      
+                # Set other parameters
                 else:
-                    print(line, end="")
-
-        # Loop through pt-hat bins and call Jetscape executable, and write output to pT-hat bin directory
-        for bin, pt_hat_min in enumerate(self.pt_hat_bins):
-
-            # Only iterate over lower bin edges
-            if bin < (len(self.pt_hat_bins) - 1):
-                pt_hat_max = self.pt_hat_bins[bin + 1]
-                print("Generating pt-hat: {} - {} ...".format(pt_hat_min, pt_hat_max))
-            else:
-                continue
+                
+                    for line in fileinput.input(xml_user_file_copy, inplace=True):
+                    
+                        if parameter_label in line:
+                            print('{}<{}>{}</{}>'.format(parameter_spacing, parameter_label, value, parameter_label))
+                        else:
+                            print(line, end='')
 
             # cd into bin directory in order to write Jetscape output there
-            output_dir_bin = "{}{}".format(self.output_dir, bin)
             os.chdir(output_dir_bin)
 
             # Run Jetscape executable
-            logfile_name = os.path.join(output_dir_bin, "log_{}.txt".format(bin))
+            logfile_name = os.path.join(output_dir_bin, "log_{}_{}.txt".format(pt_hat_bin, dir_label))
             with open(logfile_name, "w") as logfile:
                 cmd = "/home/jetscape-user/JETSCAPE/build/runJetscape jetscape_user.xml jetscape_master.xml"
                 subprocess.run(cmd, check=True, shell=True, stdout=logfile)
             os.chdir(self.output_dir)
-
 
 ##################################################################
 if __name__ == "__main__":
