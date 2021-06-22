eba311d39aea7bf35a52c17422733b900ccb11bc
Raymond Ehlers <raymond.ehlers@gmail.com>
2021-06-22 12:24:34 +0200

    Update testing outputs for writer

--- testing_outputs/finalStateWriter/compare.py
+++ testing_outputs/finalStateWriter/compare.py
@@ -11,14 +11,14 @@ import IPython
 def compare(filename: Path) -> None:
 
     new_output = ak.from_parquet(filename)
-    reference_output = ak.from_parquet(filename.parent / (str(filename.stem).replace("_00", "") + "_reference_00.parquet"))
+    reference_output = ak.from_parquet(filename.parent / (str(filename.name).replace("_00.parquet", "") + "_reference_00.parquet"))
 
     for field in ak.fields(new_output):
         print(f"Testing field: {field}")
-        np.testing.assert_allclose(np.asarray(ak.flatten(new_output[field], axis=None)), np.asarray(ak.flatten(reference_output[field], axis=None)), atol=1e-3, rtol=1e-5)
+        np.testing.assert_allclose(np.asarray(ak.flatten(new_output[field], axis=None)), np.asarray(ak.flatten(reference_output[field], axis=None)), atol=1e-3, rtol=5e-5)
 
 if __name__ == "__main__":
     print("*** hadrons ***")
-    compare(filename=Path("test_out_final_state_hadrons_00.parquet"))
+    compare(filename=Path("test_out_0001_final_state_hadrons_00.parquet"))
     print("*** partons ***")
-    compare(filename=Path("test_out_final_state_partons_00.parquet"))
+    compare(filename=Path("test_out_0001_final_state_partons_00.parquet"))
--- testing_outputs/finalStateWriter/compare.sh
+++ testing_outputs/finalStateWriter/compare.sh
@@ -6,10 +6,10 @@ cd ../../
 
 source .venv/bin/activate
 
-python3 jetscape_analysis/analysis/reader/skim_ascii.py -i /home/rehlers/code/jetscape/stat-xsede-2021/containers/test_build/jetscape_stat/build/test_out_final_state_hadrons.dat -o $comparisonDir/test_out_final_state_hadrons.parquet
-python3 jetscape_analysis/analysis/reader/skim_ascii.py -i /home/rehlers/code/jetscape/stat-xsede-2021/containers/test_build/jetscape_stat/build/test_out_final_state_partons.dat -o $comparisonDir/test_out_final_state_partons.parquet
-python3 jetscape_analysis/analysis/reader/skim_ascii.py -i /home/rehlers/code/jetscape/stat-xsede-2021/containers/test_build/jetscape_stat/build/test_out_final_state_hadrons_reference.dat -o $comparisonDir/test_out_final_state_hadrons_reference.parquet
-python3 jetscape_analysis/analysis/reader/skim_ascii.py -i /home/rehlers/code/jetscape/stat-xsede-2021/containers/test_build/jetscape_stat/build/test_out_final_state_partons_reference.dat -o $comparisonDir/test_out_final_state_partons_reference.parquet
+python3 jetscape_analysis/analysis/reader/skim_ascii.py -i /home/rehlers/code/jetscape/stat-xsede-2021/containers/test_build/jetscape_stat/build/test_out_0001_final_state_hadrons.dat -o $comparisonDir/test_out_0001_final_state_hadrons.parquet
+python3 jetscape_analysis/analysis/reader/skim_ascii.py -i /home/rehlers/code/jetscape/stat-xsede-2021/containers/test_build/jetscape_stat/build/test_out_0001_final_state_partons.dat -o $comparisonDir/test_out_0001_final_state_partons.parquet
+python3 jetscape_analysis/analysis/reader/skim_ascii.py -i /home/rehlers/code/jetscape/stat-xsede-2021/containers/test_build/jetscape_stat/build/test_out_0001_final_state_hadrons_reference.dat -o $comparisonDir/test_out_0001_final_state_hadrons_reference.parquet
+python3 jetscape_analysis/analysis/reader/skim_ascii.py -i /home/rehlers/code/jetscape/stat-xsede-2021/containers/test_build/jetscape_stat/build/test_out_0001_final_state_partons_reference.dat -o $comparisonDir/test_out_0001_final_state_partons_reference.parquet
 
 cd -
 
