7c0f547abc3066d4f6b7354a2ace64939bfd9aa5
LipeiDu <lpdu@gmail.com>
2025-02-04 16:05:54 -0800

    Consider an optionally written centrality in the final state header

--- jetscape_analysis/analysis/reader/parse_ascii.py
+++ jetscape_analysis/analysis/reader/parse_ascii.py
@@ -45,6 +45,7 @@ class HeaderInfo:
     event_plane_angle: float = attrs.field()
     n_particles: int = attrs.field()
     event_weight: float = attrs.field(default=-1)
+    centrality: float = attrs.field(default=-1)
     pt_hat: float = attrs.field(default=-1)
     vertex_x: float = attrs.field(default=-999)
     vertex_y: float = attrs.field(default=-999)
@@ -236,25 +237,43 @@ def _parse_header_line_format_v2(line: str) -> HeaderInfo:
     # Compare by length first so we can short circuit immediately if it doesn't match, which should
     # save some string comparisons.
     info: Union[HeaderInfo, CrossSection]
-    if (len(values) == 9 or len(values) == 11) and values[1] == "Event":
+    if (len(values) == 9 or len(values) == 11 or len(values) == 13) and values[1] == "Event":
         ##########################
         # Header v2 specification:
         ##########################
         # As of 22 June 2021, the formatting of the header is as follows:
         # This function was developed to parse it.
         # The header is defined as follows, with each entry separated by a `\t` character:
-        # `# Event 1 weight 0.129547 EPangle 0.0116446 N_hadrons 236 (pt_hat 47)`
-        #  0 1     2 3      4        5       6         7         8
-        # NOTE: pt_hat is optional
+        # `# Event 1 weight 0.129547 EPangle 0.0116446 N_hadrons 236 (centrality 12.5) (pt_hat 47)`
+        #  0 1     2 3      4        5       6         7         8         9         10
+        # NOTE: pt_hat and centrality are optional (centrality added in Jan. 2025)
         #
         pt_hat = -1.
-        if values[-2] == "pt_hat":
-            pt_hat = float(values[-1])
+        centrality = -1.
+        
+        # Check for the presence of pt_hat and centrality
+        if len(values) == 13:
+            # Both centrality and pt_hat are present
+            if values[-4] == "centrality":
+                centrality = float(values[-3])
+            if values[-2] == "pt_hat":
+                pt_hat = float(values[-1])
+        elif len(values) == 11:
+            # Only one of centrality or pt_hat is present
+            if values[-2] == "centrality":
+                centrality = float(values[-1])
+            elif values[-2] == "pt_hat":
+                pt_hat = float(values[-1])
+        elif len(values) == 9:
+            # Neither centrality nor pt_hat is present
+            pass
+        
         info = HeaderInfo(
             event_number=int(values[2]),            # Event number
             event_plane_angle=float(values[6]),     # EP angle
             n_particles=int(values[8]),             # Number of particles
             event_weight=float(values[4]),          # Event weight
+            centrality=centrality,                  # centrality
             pt_hat=pt_hat                           # pt hat
         )
     elif len(values) == 5 and values[1] == "sigmaGen":
@@ -690,6 +709,8 @@ def read(filename: Union[Path, str], events_per_chunk: int, parser: str = "panda
         }
         if chunk_generator.headers[0].event_weight > -1:
             header_level_info["event_weight"] = np.array([header.event_weight for header in chunk_generator.headers], np.float32)
+        if chunk_generator.headers[0].centrality > -1:
+            header_level_info["centrality"] = np.array([header.centrality for header in chunk_generator.headers], np.float32)
         if chunk_generator.headers[0].pt_hat > -1:
             header_level_info["pt_hat"] = np.array([header.pt_hat for header in chunk_generator.headers], np.float32)
         if chunk_generator.headers[0].vertex_x > -999:
